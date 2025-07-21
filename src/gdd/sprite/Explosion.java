41ef9a34b31cb6a0040938380a76d3294a1ee8b5
ShannEstabillo <u6510099@au.edu>
2025-07-21 20:24:02 +0700

    Add files via upload
    ADDED COLLISION

--- src/gdd/sprite/Explosion.java
+++ src/gdd/sprite/Explosion.java
@@ -2,31 +2,22 @@ package gdd.sprite;
 
 import static gdd.Global.*;
 import javax.swing.ImageIcon;
+import java.awt.Image;
 
 public class Explosion extends Sprite {
 
 
-    public Explosion(int x, int y) {
-
-        initExplosion(x, y);
-    }
-
-    private void initExplosion(int x, int y) {
+    public Explosion(int x, int y, Image explosionImage) {
 
         this.x = x;
         this.y = y;
-
-        var ii = new ImageIcon(IMG_EXPLOSION);
-
-        // Scale the image to use the global scaling factor
-        var scaledImage = ii.getImage().getScaledInstance(ii.getIconWidth() * SCALE_FACTOR,
-                ii.getIconHeight() * SCALE_FACTOR,
-                java.awt.Image.SCALE_SMOOTH);
-        setImage(scaledImage);
+        setImage(explosionImage);
+        visibleFrames = 30;
     }
 
-    public void act(int direction) {
-
+    @Override
+    public void act() {
+        visibleCountDown();
         // this.x += direction;
     }
 
